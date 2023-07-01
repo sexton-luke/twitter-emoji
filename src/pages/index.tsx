ea562ac457982e83be16a375ac1834a56006acab
Luke Sexton <luke@windows.local>
2023-07-01 22:23:04 +1000

    Post loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,9 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -21,7 +22,15 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
-    }
+    },
+    onError: (error) => {
+      const errorMessage = error.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
+    },
   });
 
   if (!user) return null;
@@ -40,9 +49,26 @@ const CreatePostWizard = () => {
       type="text"
       value={input}
       onChange={(event) => setInput(event.target.value)}
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          if (input !== "") {
+            mutate({ content: input });
+          }
+        }
+      }}
       disabled={isPosting}
     />
-    <button onClick={() => mutate({ content: input })}>Post</button>
+    {input !== "" && !isPosting &&
+      <button onClick={() => mutate({ content: input })} disabled={isPosting}>
+        Post
+      </button>
+    }
+    {isPosting &&
+      <div className="flex items-center justify-center">
+        <LoadingSpinner size={20} />
+      </div>
+    }
   </div>
 }
 
