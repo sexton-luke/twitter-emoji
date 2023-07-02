842cd1920755acbc12b6b0340316310020d658fa
Luke Sexton <luke@windows.local>
2023-07-02 15:57:01 +1000

    Add single post page/fetching functionality

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,11 @@
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
-import { createServerSideHelpers } from '@trpc/react-query/server';
-import { appRouter } from "~/server/api/root";
-import { prisma } from "~/server/db"
-import superjson from 'superjson';
 import { PageLayout } from "~/components/pageLayout";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
 import { PostView } from "~/components/postView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
 
 type ProfileFeedProps = {
@@ -65,22 +62,17 @@ const ProfilePage: NextPage<ProfilePageProps> = ({ username }) => {
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const helpers = createServerSideHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson, // optional - adds superjson serialization
-  });
-
+  const ssg = generateSSGHelper();
   const slug = context.params?.slug;
 
   if (typeof slug !== 'string') throw new Error("no slug");
 
   const username = slug.replace("@", "");
-  await helpers.profile.getUserByUsername.prefetch({ username });
+  await ssg.profile.getUserByUsername.prefetch({ username });
 
   return {
     props: {
-      trpcState: helpers.dehydrate(),
+      trpcState: ssg.dehydrate(),
       username,
     },
   }
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,21 +1,52 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
 import { PageLayout } from "~/components/pageLayout";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { PostView } from "~/components/postView";
 
-const SinglePostPage: NextPage = () => {
+type SinglePostPageProps = {
+  id: string;
+}
+
+const SinglePostPage: NextPage<SinglePostPageProps> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({ id });
+
+  if (!data) return <div>404</div>;
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - ${data.author.username}`}</title>
       </Head>
       <PageLayout>
-        <div>
-          Posts View
-        </div>
+        <PostView {...data} />
       </PageLayout>
     </>
-
   );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+  const id = context.params?.id;
+
+  if (typeof id !== 'string') throw new Error("no slug");
+
+  await ssg.posts.getById.prefetch({ id })
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  }
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  }
 }
 
 export default SinglePostPage;
