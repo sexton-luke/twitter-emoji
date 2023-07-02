d40d591fb4a32f04f18f7dd47169c6e6e45e4b8b
Luke Sexton <luke@windows.local>
2023-07-02 15:06:38 +1000

    Add profile header

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -6,6 +6,7 @@ import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db"
 import superjson from 'superjson';
 import { PageLayout } from "~/components/pageLayout";
+import Image from "next/image";
 
 type PageProps = {
   username: string;
@@ -22,9 +23,20 @@ const ProfilePage: NextPage<PageProps> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>
-          {data.username}
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] rounded-full border-4 border-black bg-black ml-4"
+          />
         </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl font-bold">
+          {`@${data.username ?? ""}`}
+        </div>
+        <div className="w-full border-b border-slate-400" />
       </PageLayout>
     </>
   );
